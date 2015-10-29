b195c6d9bea25e363fd8c05fe7d21355c58274ab
KasturiDona <kasturighosh@Kasturis-MacBook-Air.local>
2015-10-29 15:11:13 +1100

    added user session, email, password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :collaborator, :skillset, :rating, :hiring_status, :account)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :collaborator, :skillset, :rating, :hiring_status, :account)
     end
 end
