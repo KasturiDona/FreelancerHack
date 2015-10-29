fc2fd17f50b3ea43c539eca07951d5cc57c13499
veebuv <veebuv@gmail.com>
2015-10-29 18:13:41 +1100

    ajax requests

--- app/assets/javascripts/apitest.js
+++ app/assets/javascripts/apitest.js
@@ -1,16 +1,68 @@
 var apiTest = function(){
 	$.ajax({
-		url: 'http://www.freelancer.com/api/users/0.1/self/jobs/',
-    headers: {
-      'Freelancer-Developer-Auth-V1': DEVELOPER_ID; FREELANCER_API,
-      "Content-Type: application/json"
-    }
+		method : 'GET',
+		dataType : 'json',
+		// data : '{"jobs[]": [1,2,3]}',
+		url: 'https://freelancer.com/api/users/0.1/users/8222390/?compact',
+	    headers: {
+	      'Freelancer-Developer-Auth-V1': '34; FLN3U493UOO0QW6G9Q101ET2CML5XAKC',
+	      'Content-Type': 'application/json'
+	    },
+	    success : function(result){
+	    	console.log(result)
+	    }
 	})
 }
 
 
 $(document).ready(function(){
+	// console.log(gon.current_user)
 
-	apiTest;
+	var baseURL = 'https://www.freelancer.com/'
+	var localURl = 'localhost:3000/'
+		$.ajaxSetup({
+        headers: { 'Freelancer-Developer-Auth-V1': 34 + ';' + 'FLN3U493UOO0QW6G9Q101ET2CML5XAKC' }
+    });
+
+    $.ajax({
+      method: "POST",
+      url: baseURL + 'api/projects/0.1/projects/',
+      contentType: "application/json",
+      data: JSON.stringify({
+      title: "I WANT PANCAKES!",
+       description: "I need a CHOCOLATE MILKSHAKE",
+       currency: {
+         id: 1
+        },
+          budget: {
+        minimum: 200
+            },
+           jobs: [
+        {
+        id: 3
+        }
+                ]
+        }),
+      dataType: 'json'
+      }).done(function(data){
+      	console.log(data)
+      })
+    .done(function(data){
+        console.log(data)
+        $.ajax({
+        	method: 'PUT',
+        	url: '/' + 'projects/' + gon.current_user.id,
+        	contentType: "application/json",
+        	data: JSON.stringify({
+            user: {
+          		user_id: gon.current_user.id,
+          		pfid: data.result.id
+            }
+        	}),
+          dataType: 'json'
+        }).done(function(d){
+          console.log(d)
+        })
+    });  
 
 })
