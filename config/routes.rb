eb616b9dbb1424360c38d143c5b576c2570195a6
djstozza <daniel.sztolcman@gmail.com>
2015-10-29 14:11:34 +1100

    add routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'session/new'
 
   root :to => 'projects#index'
   resources :jobs
   resources :projects
   resources :users
+
+  get '/session' => 'session#show'
+  get '/login' => 'session#new'
+  get '/session' => 'session#show'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
   
 end
